e81445930edc8c1782370b3d06472606ad542516
Matija Šiljeg <115492643+msiljeg1997@users.noreply.github.com>
2023-08-09 16:20:20 +0200

    dodan servis

--- src/app/predavaci/predavaci.component.ts
+++ src/app/predavaci/predavaci.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Predavaci } from '../models/predavaci';
 import { PredavaciService } from '../api.service';
 
@@ -6,7 +6,8 @@ import { PredavaciService } from '../api.service';
 @Component({
   selector: 'app-predavaci',
   templateUrl: './predavaci.component.html',
-  styleUrls: ['./predavaci.component.scss']
+  styleUrls: ['./predavaci.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PredavaciComponent implements OnInit {
   participants: Predavaci[] = [];
@@ -14,8 +15,35 @@ export class PredavaciComponent implements OnInit {
   constructor(private predavaciService: PredavaciService) { }
 
   ngOnInit(): void {
-    this.predavaciService.getPredavaci().subscribe(data => {
-      this.participants = data.filter(participant => participant.disabled !== true);
-    });
+    this.loadParticipants();
   }
+  
+  loadParticipants() {
+    this.predavaciService.getPredavaci().subscribe(
+      (response: any) => {
+        if (this.isApiResponseValid(response)) {
+          this.participants = response.data;
+        } else {
+          console.error('API response daje error, ivek je nesto sjebao <3', response);
+        }
+      },
+      (error) => {
+        this.handleApiError(error);
+      }
+    );
+  }
+  
+  private isApiResponseValid(response: any): boolean {
+    return response.status === 'OK' && response.msg === 'request_success' && Array.isArray(response.data);
+  }
+  
+  private handleApiError(error: any): void {
+    if (error.status) {
+      console.error(`API req failed sa statusom: ${error.status}:`, error);
+    } else {
+      console.error('Dogodilo se sranje (error):', error);
+    }
+  }
+  
 }
+  
