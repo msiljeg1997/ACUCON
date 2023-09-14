d617337f25ab129f616ca91243dd5e4d467796aa
Matija Šiljeg <115492643+msiljeg1997@users.noreply.github.com>
2023-09-14 13:25:59 +0200

    promjene

--- src/app/naslovna/naslovna.component.ts
+++ src/app/naslovna/naslovna.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-naslovna',
@@ -8,12 +9,26 @@ import { AfterViewInit, Component } from '@angular/core';
 export class NaslovnaComponent implements AfterViewInit {
   showContent: boolean | undefined;
 
-  constructor(){}
+  constructor(private translate: TranslateService) {}
 
 // simuliranai deelay radi konzistencije 
  ngAfterViewInit() {
     setTimeout(() => {
       this.showContent = true;
     }, 700); 
+  }
+
+  getImagePath(): string {
+    // Get the current language from the TranslateService
+    const currentLang = this.translate.currentLang;
+
+    // Define the image paths based on language
+    const imagePaths: { [key: string]: string } = {
+      en: 'assets/ACU.jpg',
+      hr: 'assets/ACUHRV.jpg'
+    };
+
+    // Return the appropriate image path based on the current language
+    return imagePaths[currentLang];
   }
 }
