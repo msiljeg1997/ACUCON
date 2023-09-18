cae40234526e15790e1127e67916006230f89355
Matija Šiljeg <115492643+msiljeg1997@users.noreply.github.com>
2023-09-18 10:54:55 +0200

    trebam radionice test

--- src/app/shopitem/shopitem.component.ts
+++ src/app/shopitem/shopitem.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iRadionice } from '../models/radionice';
+import { APIServis } from '../api.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-shopItems',
   templateUrl: './shopitem.component.html',
   styleUrls: ['./shopitem.component.scss']
 })
-export class ShopitemComponent {
+export class ShopitemComponent implements OnInit {
+  radionice: iRadionice[] = [];
+
+  constructor(private radioniceService: APIServis, private translate: TranslateService ) {}
+  
+  ngOnInit(): void {
+  this.loadRadionice();
+  }
+
+
+  loadRadionice() {
+    this.radioniceService.getRadionice(this.translate.currentLang).subscribe(
+      (response: any) => {
+        if (this.isApiResponseValid(response)) {
+          this.radionice = response.data;
+        } else {
+          this.handleApiError('Invalid API response', response);
+        }
+      },
+      (error) => {
+        this.handleApiError('API request failed', error);
+      }
+    );
+  }
+
+
+  //error handling functions section
+  
+  private isApiResponseValid(response: any): boolean {
+    return (
+      response &&
+      response.status === 'OK' &&
+      response.msg === 'request_success' &&
+      Array.isArray(response.data)
+    );
+  }
+  
+  private handleApiError(message: string, error: any): void {
+    const errorMessage = error.status
+      ? `API request failed with status ${error.status}`
+      : 'An error occurred';
+  
+    console.error(`${message}:`, errorMessage, error);
+  }
 
 }
