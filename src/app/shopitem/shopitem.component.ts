778f601bd4536ebafd64e75cf02a329460d108f2
Matija Šiljeg <115492643+msiljeg1997@users.noreply.github.com>
2023-09-24 22:36:38 +0200

    show more/less added

--- src/app/shopitem/shopitem.component.ts
+++ src/app/shopitem/shopitem.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } fro
 import { iRadionice } from '../models/radionice';
 import { APIServis } from '../api.service';
 import { TranslateService } from '@ngx-translate/core';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-shopItems',
@@ -13,12 +14,16 @@ export class ShopitemComponent {
   @Input() radionice?: iRadionice;
   @Output() cardSelected = new EventEmitter<string>();
   isSelected: boolean = false;
+  isExpanded: boolean = false;
+  showMoreMobile: boolean = false;
 
 
 
-  constructor(private translate: TranslateService) {
+
+
+  constructor(private translate: TranslateService, private renderer: Renderer2) {
   }
-
+  
 
   addToBasket() {
     const cardTitle = this.radionice?.theme;
@@ -30,5 +35,15 @@ export class ShopitemComponent {
   toggleSelected() {
     this.isSelected = !this.isSelected;
   }
-
+  toggleExpanded() {
+    this.isExpanded = !this.isExpanded;
+    const cardTextElement = document.querySelector('.pTextDiv p.card-body') as HTMLElement;
+    if (this.isExpanded) {
+      this.renderer.removeStyle(cardTextElement, 'max-height');
+      this.renderer.removeStyle(cardTextElement, 'overflow');
+    } else {
+      this.renderer.setStyle(cardTextElement, 'max-height', '500px');
+      this.renderer.setStyle(cardTextElement, 'overflow', 'hidden');
+    }
+  }
 }
